9fb47246600cad795935e528934027699724e13b
nannakkj <nannaprograma@gmail.com>
2025-09-18 11:04:56 -0300

    css-end

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,30 +2,34 @@ import './Header.css'
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell, faHouse, faCompass, faComments } from "@fortawesome/free-solid-svg-icons";
+
 import whiteBubbling from '../../assets/white_icon_bubbling.png'
 import blackBubbling from '../../assets/black_icon_bubbling.png'
 
 export default function Header() {
     return (
         <header>
-            <div>
-                <img src='black-logo.png' alt="logo" />
-                <p className="teste">BUBBLE</p>
-            </div>
-            <nav>
-                <ul>
-                    <li><FontAwesomeIcon icon={faHouse} /> Início</li>
-                    <li><img src={blackBubbling} alt="Borbulhando" /> Borbulhando</li>
-                    <li><FontAwesomeIcon icon={faCompass} /> Explorar</li>
-                    <li><FontAwesomeIcon icon={faComments} /> Bulhufas</li>
-                    <li><FontAwesomeIcon icon={faBell} /> Flops</li>
-                </ul>
+            <div className='content'>
+                <div className='logo_content'>
+                    <img src='white-logo.png' alt="logo" />
+                    <p className="teste">BUBBLE</p>
+                </div>
+                <nav>
+                    <ul className='nav' id='pages_nav'>
+                        <li><FontAwesomeIcon icon={faHouse} className='text-[#E5E5E5] '/> Início</li>
+                        <li><img src={whiteBubbling} alt="Borbulhando" /> Borbulhando</li>
+                        <li><FontAwesomeIcon icon={faCompass} className='text-[#E5E5E5] '/> Explorar</li>
+                        <li><FontAwesomeIcon icon={faComments} className='text-[#E5E5E5] '/> Bulhufas</li>
+                        <li className='mt-80'><FontAwesomeIcon icon={faBell} className='text-[#E5E5E5] '/> Flops</li>
+                    </ul>
 
-                <ul>
-                    <li><ion-icon name="person-circle"></ion-icon>Seu Perfil</li>
-                    <li><ion-icon name="settings-outline"></ion-icon>Configurações</li>
-                </ul>
-            </nav>
+                    <ul className='nav' id='person_nav'>
+                        <li><ion-icon name="person-circle" className='text-[#E5E5E5]'></ion-icon>Seu Perfil</li>
+                        <li><ion-icon name="settings-outline" className='text-[#E5E5E5]'></ion-icon>Configurações</li>
+                    </ul>
+                </nav>
+            </div>
         </header>
+        
     );
 }
