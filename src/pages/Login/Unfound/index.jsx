36be9e829110fca3333b74de62ecf5c4e61fb052
nannakkj <nannaprograma@gmail.com>
2025-10-06 21:00:59 -0300

    other login pages

--- src/pages/Login/Unfound/index.jsx
+++ src/pages/Login/Unfound/index.jsx
@@ -9,17 +9,18 @@ export default function Unfound() {
             <img src={bg2} className="half-image" />
             <div className='login-main'>
                 <div className='login-container'>
-                    <div className="container">
+                    <div className="container" id='forgot-main'>
                         <ButtonGoBack />
-                        <div className="logo-content">
+                        <div className="logo-content" id='logo-forgot'>
                             <img src="../../white-logo.png" alt="" className='bubble_img'/><p>Bubble</p>
                         </div>
-
-                        <form className='login-form' action={`http://localhost:5173/login/forgot`}>
-                            <h1>Falha na localização do Email</h1>
-                            <h3>Infelizmente não encontramos seu email em nosso Banco de Dados. Por favor, verifique novamente o endereço do email e tente novamente.</h3>
-                            <input type="submit" value="Continuar" className='cursor-pointer'/>
-                        </form>
+                        <div className='form_login'>
+                            <form className='login-form' action={`http://localhost:5173/login/forgot`}>
+                                <h1>Falha na localização do Email</h1>
+                                <h3>Infelizmente não encontramos seu email em nosso Banco de Dados. Por favor, verifique novamente o endereço do email e tente novamente.</h3>
+                                <input type="submit" value="Continuar" className='cursor-pointer'/>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
