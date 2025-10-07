e8f94a274257618e043119bd7ae6bd125757bb8a
felipinho3105 <fhsgamer123@gmail.com>
2025-10-07 15:29:14 -0300

    só falta o css matheus >:(

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,8 +3,14 @@ import Header from '../../components/Header'
 import Post from '../../components/Postagem'
 import Status from '../../components/Status'
 import Aside from '../../components/Aside'
-import ftPost from'../../assets/post-Yasmin.png'
-import ftPerfil from '../../assets/Profile-picture.png'
+import ftPost from'../../assets/post.png'
+import ftPerfil from '../../assets/perfil.png'
+import ftPost1 from'../../assets/post1.jpeg'
+import ftPerfil1 from '../../assets/perfil1.jpg'
+import ftPost2 from'../../assets/post2.jpg'
+import ftPerfil2 from '../../assets/perfil2.jpg'
+import ftPost3 from'../../assets/post3.png'
+import ftPerfil3 from '../../assets/perfil3.jpg'
 
 function Home() {
   return (
@@ -23,13 +29,29 @@ function Home() {
       like_num="9.2K"
       com_num="2.1K"
       />
-      <Post name=""
-      id=""
-      description=""
-      url_image_perfil={" "}
-      url_image_post={" "}
-      like_num=""
-      com_num=""
+      <Post name="NoobMaster"
+      id="@Noob_master123"
+      description="Esse novo SilkSong tá demais!!! #SilkSong #hashtag #turururu"
+      url_image_perfil={ftPerfil1}
+      url_image_post={ftPost1}
+      like_num="2.9K"
+      com_num="759"
+      />
+      <Post name="Joãozinho.html"
+      id="@Joaozinho.html"
+      description="Esse AP em Caraguatatuba tem uma das melhores vistas que eu já tive! #viagens"
+      url_image_perfil={ftPerfil2}
+      url_image_post={ftPost2}
+      like_num="433"
+      com_num="38"
+      />
+      <Post name="Davi Britto"
+      id="@davibritto"
+      description="Mais um dia abençoado pelo nosso Senhor Jesus Cristo! #Deus #Abençoado #DaviBritto #calabreso"
+      url_image_perfil={ftPerfil3}
+      url_image_post={ftPost3}
+      like_num="439.2K"
+      com_num="57.7K"
       />
     </main>
     
