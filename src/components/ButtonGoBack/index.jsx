f4f36184b64f918f337497b5a9b86c54a44ef573
Hydrazl <joteixeirasimoes@gmail.com>
2025-10-15 14:21:36 -0300

    📌 Inserção ao Git Pages

--- src/components/ButtonGoBack/index.jsx
+++ src/components/ButtonGoBack/index.jsx
@@ -1,7 +1,5 @@
-import React from "react";
 import './ButtonGoBack.css';
 import { useGoBack } from "../../hook/useGoBack";
-import { IoIosArrowRoundForward } from "react-icons/io";
 
 export default function ButtonGoBack({ fallbackPath = '/' }) {
         const goBack = useGoBack (fallbackPath);
@@ -9,8 +7,7 @@ export default function ButtonGoBack({ fallbackPath = '/' }) {
         return (    
             <button onClick={goBack}>
                 <div className='circle-icon cursor-pointer'>
-                    <IoIosArrowRoundForward />
-                    {/* <ion-icon name="arrow-forward-outline"></ion-icon> */}
+                    <ion-icon name="arrow-forward-outline"></ion-icon>
                 </div>
             </button>
         );
