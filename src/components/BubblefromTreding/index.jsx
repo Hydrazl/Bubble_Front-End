863953ad6e5122b9931169a09e712bf4b98d1a50
Mano Baruru <baruki.henri@aluno.ifsp.edu.br>
2025-11-02 14:56:24 -0300

    css feito

--- src/components/BubblefromTreding/index.jsx
+++ src/components/BubblefromTreding/index.jsx
@@ -1,6 +1,7 @@
 import './bubblefromTreding.css'
 
-export default function BubblefromTreding({img1,img2, img3, img4, img5, img6, img7, img8, img9,img10}){
+
+export default function BubblefromTreding({img1,img2, img3, img4, img5, img6, img7, img8, img9}){
     return(
         <>
            <div className='BubbleGroup'>
@@ -9,40 +10,36 @@ export default function BubblefromTreding({img1,img2, img3, img4, img5, img6, im
                     <img src={img1}/>
                 </div>
 
-                <div className='BubbleImg'>
+                <div className='BubbleImg' id='position'>
                     <img src={img2}/>
                 </div>
 
-                <div className='BubbleImg'>
+                <div className='BubbleImg' id='position2'>
                     <img src={img3}/>
                 </div>
 
-                <div className='BubbleImg'>
+                <div className='BubbleImg' id='position3'>
                     <img src={img4}/>
                 </div>
 
-                <div className='BubbleImg'>
+                <div className='BubbleImg' id='position4'>
                     <img src={img5}/>
                 </div>
 
-                <div className='BubbleImg'>
+                <div className='BubbleImg' id='position5'>
                     <img src={img6}/>
                 </div>
 
-                <div className='BubbleImg'>
+                <div className='BubbleImg' id='position6'>
                     <img src={img7}/>
                 </div>
 
-                <div className='BubbleImg'>
+                <div className='BubbleImg' id='position7'>
                     <img src={img8}/>
                 </div>
 
-                <div className='BubbleImg'>
+                <div className='BubbleImg' id='position8'>
                     <img src={img9}/>
-                </div>
-
-                <div className='BubbleImg'>
-                    <img src={img10}/>
                 </div>
 
            </div>
@@ -50,3 +47,4 @@ export default function BubblefromTreding({img1,img2, img3, img4, img5, img6, im
         </>
     )
 }
+
