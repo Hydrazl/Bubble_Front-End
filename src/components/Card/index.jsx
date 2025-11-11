61b1d31cc737c0b80889545b87ce078baa6bd475
ktzxs <lucasalvesdacosta070@gmail.com>
2025-11-11 16:38:27 -0300

    Desenvolvendo a explore 🦀

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,6 +1,4 @@
 import './Card.css'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUsers } from "@fortawesome/free-solid-svg-icons";
 
 export default function Card({ title, image }) {
   return (
@@ -8,9 +6,24 @@ export default function Card({ title, image }) {
       <h1 className="title">
         {title}
       </h1>
-      <div className="card-image-wrapper">
-        <img src={image} alt="card-image" className="card-image" />
-      </div>
+
+      <div className="imagens">
+        <div className="card-image-wrapper">
+            <img src={image} alt="card-image" className="card-image" />
+        </div>
+
+        <div className="card-image-wrapper">
+            <img src={image} alt="card-image" className="card-image" />
+        </div>
+
+        <div className="card-image-wrapper">
+            <img src={image} alt="card-image" className="card-image" />
+        </div>
+
+        <div className="card-image-wrapper">
+            <img src={image} alt="card-image" className="card-image" />
+        </div>
+        </div>
     </div>
   );
 }
--- src/pages/Explorer/index.jsx
+++ src/pages/Explorer/index.jsx
@@ -1,14 +1,32 @@
-import './Explorer.css'
-import Header from '../../components/Header'
+import './Explorer.css'; 
+import Status from '../../components/Status';
+import Header from '../../components/Header'; 
+import Aside from '../../components/Aside'; 
+import Card from '../../components/Card';
+import Carro from '../../assets/carro-explorer.jpg';
 
-function Explorer() {
+ function Explorer() { 
   return (
-    <>
-    <header>
-      <Header/>
-    </header>
+    <>     
+      <header>       
+        <Header />     
+      </header>      
+    
+      <main>       
+        <Status />     
+
+        <Card
+           title="Mais Populares"
+           image={Carro}
+           
+        />
+      </main>
+
+      <aside>
+        <Aside />
+      </aside>   
     </>
-  )
-}
+  ) 
+} 
 
 export default Explorer
