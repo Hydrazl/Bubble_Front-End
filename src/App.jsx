4d683cdc9ff60be1cc11f012de436b26e64c0d35
Hydrazl <joteixeirasimoes@gmail.com>
2025-11-29 15:54:00 -0300

    Puxando o Post para a home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import AppRoutes from './routes'
 import { ThemeProvider } from './utils/theme/ThemeContext'
+import { PostProvider } from './context/PostContext'
 
 function App() {
   return (
     <ThemeProvider>
-      <AppRoutes />
+      <PostProvider>
+        <AppRoutes />
+      </PostProvider>
     </ThemeProvider>
   )
 }
