41df351a1d5e4a063c74750231c5e538c6dd8c00
ktzxs <lucasalvesdacosta070@gmail.com>
2025-11-29 11:20:06 -0300

    criado a contagem de like, acho que vai dar problema na rota ( não tenho certeza ).

--- src/components/LikeButton/index.jsx
+++ src/components/LikeButton/index.jsx
@@ -1,26 +1,44 @@
 import { useState } from "react";
-import { IoHeart, IoHeartOutline  } from "react-icons/io5";
-import "./LikeButton.css";
+import { IoHeart, IoHeartOutline } from "react-icons/io5";
+import axios from "axios";
 
-export default function LikeButton({ initialLiked = false, onToggle}) {
+export default function LikeButton({ postId, userId, initialLiked = false }) {
     const [liked, setLiked] = useState(initialLiked);
     const [isAnimating, setIsAnimating] = useState(false);
 
-    const handleClick = () => {
+    const handleClick = async () => {
         const newValue = !liked;
         setLiked(newValue);
 
         if (newValue) {
-        setIsAnimating(true);
-        setTimeout(() => setIsAnimating(false), 500);
+            setIsAnimating(true);
+            setTimeout(() => setIsAnimating(false), 500);
         }
-        
-        if (onToggle) onToggle(newValue);
+
+        try {
+            const response = await axios.post("http://localhost:4000/like/toggle", {
+                postId,
+                userId
+            });
+
+            // vamo garantir que vai no backend:
+            setLiked(response.data.liked);
+
+        } catch (err) {
+            console.error("Erro ao enviar like:", err);
+            setLiked(liked);
+        }
     };
 
     return (
-        <button onClick={handleClick} className={`text-4xl cursor-pointer transition-transform duration-200 ease-out ${liked ? "scale-125 text-red-500" : "scale-100 text-gray-400"} ${isAnimating ? "animate-like" : ""}`}>
-            {liked ? (<IoHeart className="animate-like-bounce" />) : <IoHeartOutline />}
+        <button 
+            onClick={handleClick} 
+            className={`text-4xl cursor-pointer transition-transform duration-200 ease-out 
+                ${liked ? "scale-125 text-red-500" : "scale-100 text-gray-400"} 
+                ${isAnimating ? "animate-like" : ""}
+            `}
+        >
+            {liked ? <IoHeart className="animate-like-bounce" /> : <IoHeartOutline />}
         </button>
     );
 }
